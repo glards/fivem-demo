17ca388112f9ce1b5f509c85713e2a780d95dfcc
Steve Glardon <sglardon@gmail.com>
2021-08-29 13:27:27 +0200

    Bootstrap resource
    Disable car radio
    Command 'info' to display current ped coordinates

---
+++ data/resources/gl_bootstrap/cl_startup.lua
@@ -0,0 +1,56 @@
+local clientIsRunning = false
+local displayInfo = false
+
+function ClientThread()
+    while clientIsRunning do
+        Citizen.Wait(0)
+
+        local ped = PlayerPedId()
+        if IsPedInAnyVehicle(ped) then
+            SetUserRadioControlEnabled(false)
+            if GetPlayerRadioStationName() ~= nil then
+                SetVehRadioStation(GetVehiclePedIsIn(ped),"OFF")
+            end
+        end
+
+        if displayInfo then
+            local coords = GetEntityCoords(ped)
+            local heading = GetEntityHeading(ped)
+            SetTextScale(0.0, 0.3)
+            SetTextOutline()
+
+            BeginTextCommandDisplayText('STRING')
+            AddTextComponentSubstringPlayerName(
+                string.format("~r~Coords :~s~~n~X: %.2f~n~Y: %.2f~n~Z: %.2f~n~H: %.2f",
+                    coords.x,
+                    coords.y,
+                    coords.z,
+                    heading)
+            )
+            EndTextCommandDisplayText(0, 0.5)
+        end
+    end
+end
+
+AddEventHandler('onResourceStart', function(resource)
+    if resource == GetCurrentResourceName() then
+        print('Starting bootstrap')
+        clientIsRunning = true
+        Citizen.CreateThread(ClientThread)
+
+        SetPoliceIgnorePlayer(ped, true)
+        SetMaxWantedLevel(0)
+    end
+end)
+
+AddEventHandler('onResourceStop', function(resource)
+    if resource == GetCurrentResourceName() then
+        print('Stopping bootstrap')
+        clientIsRunning = false
+    end
+end)
+
+
+RegisterCommand('info', function()
+    displayInfo = not displayInfo
+end, false)
