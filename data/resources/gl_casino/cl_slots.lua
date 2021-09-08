1a249202f56ba75f6805f132a80f15c925d5b137
Steve Glardon <sglardon@gmail.com>
2021-09-08 21:36:45 +0200

    Added cl_slots

---
+++ data/resources/gl_casino/cl_slots.lua
@@ -0,0 +1,307 @@
+
+wheelOffset = vector3(0.0, -0.12, 1.6)
+
+slotOffsets = {
+    vector3(-0.115, 0.047, 0.906),
+    vector3(0.005, 0.047, 0.906),
+    vector3(0.125, 0.047, 0.906)
+}
+
+seats = {
+    vector3(1101.01, 229.876, -50.8409),
+    vector3(1101.59, 230.626, -50.8409),
+    vector3(1101.95, 231.501, -50.8409),
+    vector3(1102.08, 232.433, -50.8409),
+    vector3(1101.96, 233.367, -50.8409),
+    vector3(1108.41, 238.946, -50.8409),
+    vector3(1109.16, 238.376, -50.8409),
+    vector3(1110.03, 238.016, -50.8409),
+    vector3(1110.97, 237.89, -50.8409),
+    vector3(1111.91, 238.012, -50.8409),
+    vector3(1112.78, 238.371, -50.8409),
+    vector3(1113.53, 238.943, -50.8409),
+    vector3(1120.13, 233.355, -50.8409),
+    vector3(1120, 232.427, -50.8409),
+    vector3(1120.13, 231.494, -50.8409),
+    vector3(1120.48, 230.624, -50.8409),
+    vector3(1121.06, 229.878, -50.8409),
+    vector3(1104.13, 228.836, -50.8409),
+    vector3(1103.59, 230.55, -50.8409),
+    vector3(1105.05, 231.597, -50.8409),
+    vector3(1106.5, 230.53, -50.8409),
+    vector3(1105.93, 228.823, -50.8409),
+    vector3(1107.56, 233.308, -50.8409),
+    vector3(1107.02, 235.023, -50.8409),
+    vector3(1108.48, 236.07, -50.8409),
+    vector3(1109.93, 235.003, -50.8409),
+    vector3(1109.36, 233.297, -50.8409),
+    vector3(1113.2, 233.067, -50.8409),
+    vector3(1112.65, 234.78, -50.8409),
+    vector3(1114.11, 235.828, -50.8409),
+    vector3(1115.56, 234.76, -50.8409),
+    vector3(1115, 233.054, -50.8409),
+    vector3(1116.22, 228.28, -50.8409),
+    vector3(1115.68, 229.995, -50.8409),
+    vector3(1117.14, 231.042, -50.8409),
+    vector3(1118.59, 229.975, -50.8409),
+    vector3(1118.02, 228.269, -50.8409),
+    vector3(1129.64, 251.203, -52.0409),
+    vector3(1130.57, 251.085, -52.0409),
+    vector3(1131.44, 250.73, -52.0409),
+    vector3(1132.19, 250.159, -52.0409),
+    vector3(1132.76, 249.412, -52.0409),
+    vector3(1133.12, 248.533, -52.0409),
+    vector3(1133.24, 247.598, -52.0409),
+    vector3(1133.42, 255.572, -52.0409),
+    vector3(1133.16, 257.251, -52.0409),
+    vector3(1134.67, 258.021, -52.0409),
+    vector3(1135.87, 256.819, -52.0409),
+    vector3(1135.1, 255.303, -52.0409),
+    vector3(1137.66, 251.328, -52.0409),
+    vector3(1137.4, 253.008, -52.0409),
+    vector3(1138.92, 253.779, -52.0409),
+    vector3(1140.12, 252.574, -52.0409),
+    vector3(1139.34, 251.061, -52.0409)
+}
+
+slotMachines = {
+    vector4( 1100.483, 230.4082, -50.8409, 45.0),
+    vector4( 1100.939, 231.0017, -50.8409, 60.0),
+    vector4( 1101.221, 231.6943, -50.8409, 75.0),
+    vector4( 1101.323, 232.4321, -50.8409, 90.0),
+    vector4( 1101.229, 233.1719, -50.8409, 105.0),
+    vector4( 1108.938, 239.4797, -50.8409, -45.0),
+    vector4( 1109.536, 239.0278, -50.8409, -30.0),
+    vector4( 1110.229, 238.7428, -50.8409, -15.0),
+    vector4( 1110.974, 238.642, -50.8409, 0.0),
+    vector4( 1111.716, 238.7384, -50.8409, 15.0),
+    vector4( 1112.407, 239.0216, -50.8409, 30.0),
+    vector4( 1112.999, 239.4742, -50.8409, 45.0),
+    vector4( 1120.853, 233.1621, -50.8409, -105.0),
+    vector4( 1120.753, 232.4272, -50.8409, -90.0),
+    vector4( 1120.853, 231.6886, -50.8409, -75.0),
+    vector4( 1121.135, 230.9999, -50.8409, -60.0),
+    vector4( 1121.592, 230.4106, -50.8409, -45.0),
+    vector4( 1104.572, 229.4451, -50.8409, -36.0),
+    vector4( 1104.302, 230.3183, -50.8409, -108.0),
+    vector4( 1105.049, 230.845, -50.8409, 180.0),
+    vector4( 1105.781, 230.2973, -50.8409, 108.0),
+    vector4( 1105.486, 229.4322, -50.8409, 36.0),
+    vector4( 1108.005, 233.9177, -50.8409, -36.0),
+    vector4( 1107.735, 234.7909, -50.8409, -108.0),
+    vector4( 1108.482, 235.3176, -50.8409, 180.0),
+    vector4( 1109.214, 234.7699, -50.8409, 108.0),
+    vector4( 1108.919, 233.9048, -50.8409, 36.0),
+    vector4( 1113.64, 233.6755, -50.8409, -36.0),
+    vector4( 1113.37, 234.5486, -50.8409, -108.0),
+    vector4( 1114.117, 235.0753, -50.8409, 180.0),
+    vector4( 1114.848, 234.5277, -50.8409, 108.0),
+    vector4( 1114.554, 233.6625, -50.8409, 36.0),
+    vector4( 1116.662, 228.8896, -50.8409, -36.0),
+    vector4( 1116.392, 229.7628, -50.8409, -108.0),
+    vector4( 1117.139, 230.2895, -50.8409, 180.0),
+    vector4( 1117.871, 229.7419, -50.8409, 108.0),
+    vector4( 1117.576, 228.8767, -50.8409, 36.0),
+    vector4( 1129.64, 250.451, -52.0409, 180.0),
+    vector4( 1130.376, 250.3577, -52.0409, 165.0),
+    vector4( 1131.062, 250.0776, -52.0409, 150.0),
+    vector4( 1131.655, 249.6264, -52.0409, 135.0),
+    vector4( 1132.109, 249.0355, -52.0409, 120.0),
+    vector4( 1132.396, 248.3382, -52.0409, 105.0),
+    vector4( 1132.492, 247.5984, -52.0409, 90.0),
+    vector4( 1133.952, 256.1037, -52.0409, -45.0),
+    vector4( 1133.827, 256.9098, -52.0409, -117.0),
+    vector4( 1134.556, 257.2778, -52.0409, 171.0),
+    vector4( 1135.132, 256.699, -52.0409, 99.0),
+    vector4( 1134.759, 255.9734, -52.0409, 27.0),
+    vector4( 1138.195, 251.8611, -52.0409, -45.0),
+    vector4( 1138.07, 252.6677, -52.0409, -117.0),
+    vector4( 1138.799, 253.0363, -52.0409, 171.0),
+    vector4( 1139.372, 252.4563, -52.0409, 99.0),
+    vector4( 1139, 251.7306, -52.0409, 27.0)
+}
+
+slotMachinesTypes = {
+    4,
+    5,
+    6,
+    7,
+    8,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    1,
+    2,
+    3,
+    4,
+    5,
+    4,
+    5,
+    1,
+    2,
+    3,
+    7,
+    8,
+    4,
+    5,
+    6,
+    4,
+    5,
+    1,
+    2,
+    3,
+    7,
+    8,
+    4,
+    5,
+    6,
+    8,
+    7,
+    6,
+    5,
+    4,
+    3,
+    2,
+    3,
+    4,
+    5,
+    1,
+    2,
+    6,
+    7,
+    8,
+    4,
+    5
+}
+
+machinesSlotWheelHashes = {
+    nil,
+    nil,
+    nil,
+    `vw_Prop_vw_slot_wheel_04a`,
+    nil,
+    nil,
+    nil,
+    `vw_Prop_vw_slot_wheel_08a`
+}
+
+machinesSlotReelHashes = {
+    `vw_Prop_Casino_Slot_01a_reels`,
+    `vw_Prop_Casino_Slot_02a_reels`,
+    `vw_Prop_Casino_Slot_03a_reels`,
+    `vw_Prop_Casino_Slot_04a_reels`,
+    `vw_Prop_Casino_Slot_05a_reels`,
+    `vw_Prop_Casino_Slot_06a_reels`,
+    `vw_Prop_Casino_Slot_07a_reels`,
+    `vw_Prop_Casino_Slot_08a_reels`,
+}
+
+function loadModels(models)
+    for i=1,#models do
+        local hash = models[i]
+        if hash ~= nil then
+            RequestModel(hash)
+            while not HasModelLoaded(hash) do
+                Citizen.Wait(0)
+            end
+        end
+    end
+end
+
+local machineObjects = {}
+local machineInstances = {}
+
+local MACHINE_SLOT = 1
+local MACHINE_WHEEL = 2
+
+Citizen.CreateThread(function ()
+    loadModels(machinesSlotReelHashes)
+    loadModels(machinesSlotWheelHashes)
+    for i=1,#slotMachines do
+        -- Game checks if player is within 10.0 units of the machine to create objects
+        local pos = slotMachines[i]
+        local machineType = slotMachinesTypes[i]
+
+        -- Wheel
+        local wheelHash = machinesSlotWheelHashes[machineType]
+        if wheelHash ~= nil then
+            local wheelPos = GetObjectOffsetFromCoords(pos, wheelOffset)
+            local o = CreateObject(wheelHash, wheelPos, false, false, true)
+            FreezeEntityPosition(o, true)
+            SetEntityCollision(o, false, false)
+            SetEntityRotation(o, 0.0, 0.0, pos.w, 2, true)
+            table.insert(machineObjects, o)
+            table.insert(machineInstances, {
+                Index = i,
+                Object = o,
+                Pos = wheelPos,
+                Heading = pos.w,
+                MachineType = machineType,
+                ObjectType = MACHINE_WHEEL
+            })
+        end
+
+        -- Reel
+        local reelHash = machinesSlotReelHashes[machineType]
+        if reelHash ~= nil then
+            for j=1,3 do
+                local reelPos = GetObjectOffsetFromCoords(pos, slotOffsets[j])
+                local o = CreateObject(reelHash, reelPos, false, false, true)
+                FreezeEntityPosition(o, true)
+                SetEntityCollision(o, false, false)
+                SetEntityRotation(o, 0.0, 0.0, pos.w, 2, true)
+                table.insert(machineObjects, o)
+                table.insert(machineInstances, {
+                    Index = i,
+                    Object = o,
+                    Pos = reelPos,
+                    Heading = pos.w,
+                    MachineType = machineType,
+                    ObjectType = MACHINE_SLOT
+                })
+            end
+        end
+    end
+
+    local speed = 12.0
+    local delay = 100
+    local next = GetGameTimer() + delay
+    local angle = 0.0
+
+    while true do
+        Citizen.Wait(0)
+
+        if GetGameTimer() > next then
+            angle = angle + speed
+            if angle > 360.0 then
+                angle = angle - 360.0
+            end 
+            next = GetGameTimer() + delay
+        end
+
+        for i=1,#machineInstances do
+            local instance = machineInstances[i]
+            if instance.ObjectType == MACHINE_SLOT then
+                SetEntityRotation(instance.Object, angle, 0.0, instance.Heading, 2, true)
+            else
+                SetEntityRotation(instance.Object, 0.0, angle, instance.Heading, 2, true)
+            end
+            
+        end
+    end
+end)
+
+
+
+AddEventHandler('onResourceStop', function(resource)
+    if resource == GetCurrentResourceName() then
+        for i=1,#machineObjects do
+            DeleteEntity(machineObjects[i])
+        end
+        machineObjects = {}
+    end
+end)
