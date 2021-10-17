ca4732c3a42fb47e87081472918135ee6bd0f37d
Steve Glardon <sglardon@gmail.com>
2021-10-17 13:10:39 +0200

    Added material / entity pointing to info command

--- data/resources/gl_bootstrap/cl_startup.lua
+++ data/resources/gl_bootstrap/cl_startup.lua
@@ -1,9 +1,15 @@
 local clientIsRunning = true
 local displayInfo = false
 
+local SET_COORDS_OFFSET = vector3(0,0,-1)
+
 function ClientThread()
     SetPoliceIgnorePlayer(ped, true)
     SetMaxWantedLevel(0)
+
+    local shapeTestHandle = nil
+    local lastMaterial = nil
+    local lastEntity = nil
 
     while clientIsRunning do
         Citizen.Wait(0)
@@ -19,18 +25,78 @@ function ClientThread()
         if displayInfo then
             local coords = GetEntityCoords(ped)
             local heading = GetEntityHeading(ped)
+
+            ShowHudComponentThisFrame(14)
+
             SetTextScale(0.0, 0.3)
             SetTextOutline()
 
             BeginTextCommandDisplayText('STRING')
             AddTextComponentSubstringPlayerName(
-                string.format("~r~Coords :~s~~n~X: %.2f~n~Y: %.2f~n~Z: %.2f~n~H: %.2f",
+                string.format("~r~Coords :~s~~n~X: %.2f~n~Y: %.2f~n~Z: %.2f~n~H: %.2f~n~",
                     coords.x,
                     coords.y,
                     coords.z,
                     heading)
             )
             EndTextCommandDisplayText(0, 0.5)
+
+            local cameraRotation = GetGameplayCamRot()
+            local cameraCoord = GetGameplayCamCoord()
+            
+            local cameraRotationRad = math.rad(cameraRotation)
+
+            local camForwardVec = norm(vector3(
+                -math.sin(cameraRotationRad.z)*math.abs(math.cos(cameraRotationRad.x)),
+                math.cos(cameraRotationRad.z)*math.abs(math.cos(cameraRotationRad.x)),
+                math.sin(cameraRotationRad.x)
+            ))
+
+            local newPos = cameraCoord+camForwardVec*20.0
+
+            if shapeTestHandle == nil then
+                shapeTestHandle = StartShapeTestLosProbe(cameraCoord.x, cameraCoord.y, cameraCoord.z, newPos.x, newPos.y, newPos.z, -1, 0, 4)
+            else
+                local retval --[[ integer ]],
+                    hit --[[ boolean ]],
+                    endCoords --[[ vector3 ]],
+                    surfaceNormal --[[ vector3 ]],
+                    materialHash --[[ Hash ]],
+                    entityHit --[[ Entity ]] = GetShapeTestResultIncludingMaterial(shapeTestHandle)
+                
+                if retval == 2 then
+                    if hit then
+                        lastMaterial = materialHash
+                        lastEntity = ""
+                        
+                        if entityHit ~= 0 then
+                            lastEntity = ""
+                            local entityType = GetEntityType(entityHit)
+                            if entityType == 1 then
+                                lastEntity = lastEntity .. "Ped " .. entityHit
+                            elseif entityType == 2 then
+                                local plate = GetVehicleNumberPlateText(entityHit)
+                                lastEntity = lastEntity .. "Vehicle " .. plate
+                            end
+                        end
+                    else
+                        lastMaterial = ""
+                        lastEntity = ""
+                    end
+
+                    shapeTestHandle = nil
+                end
+            end
+
+            SetTextScale(0.0, 0.3)
+            SetTextOutline()
+            BeginTextCommandDisplayText('STRING')
+            AddTextComponentSubstringPlayerName(
+                string.format("MAT: %s~n~ENT: %s",
+                    lastMaterial,
+                    lastEntity)
+            )
+            EndTextCommandDisplayText(0, 0.15)
         end
     end
 end
