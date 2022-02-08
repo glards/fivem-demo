96fe26c0d9270c60e1b6780dba1947877f17e851
Glards <sglardon@gmail.com>
2022-02-09 00:09:46 +0100

    More utility commands

--- data/resources/gl_admin/cl_commands.lua
+++ data/resources/gl_admin/cl_commands.lua
@@ -11,3 +11,20 @@ RegisterCommand('unstuck', function(source, args, rawCommand)
     TaskPlayAnim(ped, 'amb@code_human_wander_smoking_fat@male@base', 'static', 8.0,8.0,-1,0,1.0,0.0,0.0,0.0)
     ClearPedTasksImmediately(ped)
 end, false)
+
+RegisterCommand('ping', function(source, args, rawCommand)
+    local ped = PlayerPedId()
+
+    local currentTimestamp = GetGameTimer()
+    TriggerServerEvent('gl_admin:ping', currentTimestamp)
+end, false)
+
+local function pong(clientTimestamp, serverTimestamp)
+    local currentTimestamp = GetGameTimer()
+
+    local rtt = currentTimestamp - clientTimestamp
+    local ping = serverTimestamp - clientTimestamp
+
+    print(string.format("RTT : %d ms | Ping : %d ms", rtt, ping))
+end
+RegisterNetEvent('gl_admin:pong', pong)
--- data/resources/gl_admin/sv_admin.lua
+++ data/resources/gl_admin/sv_admin.lua
@@ -7,3 +7,12 @@ RegisterCommand('identifiers', function(source)
         print('Player identifier #' .. i .. ': ' .. identifier)
     end
 end, false)
+
+
+local function ping(clientTimestamp)
+    local src = source
+
+    local currentTimestamp = GetGameTimer()
+    TriggerClientEvent('gl_admin:pong', src, clientTimestamp, currentTimestamp)
+end
+RegisterNetEvent('gl_admin:ping', ping)
